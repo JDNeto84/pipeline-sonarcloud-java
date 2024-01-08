9b989cfa5195d752458ab32884c487bf8beb1a7e
JD-Neto <jdneto84@gmail.com>
2024-01-07 21:49:04 -0300

    add ajust

--- src/main/java/org/javaTestUnit/Main.java
+++ src/main/java/org/javaTestUnit/Main.java
@@ -1,15 +1,14 @@
 package org.javaTestUnit;
-
 public class Main {
     public static void main(String[] args) {
+
         Integer n1 = 11, n2 = 11;
         Main calculation = new Main();
         Integer resultAddition = calculation.addition(n1, n2);
         System.out.println("Result of Addition: " + resultAddition);
-    }
 
+    }
     public Integer addition(Integer n1, Integer n2) {
         return n1 + n2;
     }
 }
-
--- src/test/java/org/javaTestUnit/MainTest.java
+++ src/test/java/org/javaTestUnit/MainTest.java
@@ -3,11 +3,13 @@ package org.javaTestUnit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
+public class MainTest {
+
     @Test
     public void addTwoNumbers(){
         Main calculation = new Main();
         Integer resultSum = calculation.addition(1, 1);
         Assertions.assertEquals(2, resultSum);
     }
+
 }
