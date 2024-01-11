5e45ebdcbe81b3d5f58bfe3cc6f2a0ed995d73ab
JD-Neto <jdneto84@gmail.com>
2024-01-10 22:19:14 -0300

    add jacoco

--- src/main/java/org/javaTestUnit/Main.java
+++ src/main/java/org/javaTestUnit/Main.java
@@ -6,7 +6,6 @@ public class Main {
         Main calculation = new Main();
         Integer resultAddition = calculation.addition(n1, n2);
         System.out.println("Result of Addition: " + resultAddition);
-
     }
     public Integer addition(Integer n1, Integer n2) {
         return n1 + n2;
